532695d04b76d947a9bb73ffe64509fe82903efa
ChathuraAbeygunawardhana <chathuraabeygunawardhana@gmail.com>
2024-10-29 09:47:37 +0530

    EA | Created bottom tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,36 @@
-import { Stack } from "expo-router";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import Home from './Home';
+import Search from './Search';
+import Cart from './Cart';
+import Profile from './Profile';
+
+const Tab = createBottomTabNavigator();
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName: 'home' | 'search' | 'cart' | 'person' | undefined;
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Search') {
+            iconName = 'search';
+          } else if (route.name === 'Cart') {
+            iconName = 'cart';
+          } else if (route.name === 'Profile') {
+            iconName = 'person';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Cart" component={Cart} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
   );
 }

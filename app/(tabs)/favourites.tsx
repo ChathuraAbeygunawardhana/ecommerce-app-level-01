601c6c3b381d60c40f5b5b4e3a0858c3a69e694b
ChathuraAbeygunawardhana <chathuraabeygunawardhana@gmail.com>
2024-11-17 19:38:22 +0530

    EA | added the favorites tab

---
+++ app/(tabs)/favourites.tsx
@@ -0,0 +1,21 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Favourites = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Favourites</Text>
+    </View>
+  );
+};
+
+export default Favourites;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

333e8ef5becc879f82d2495d07d053c57bf0adf6
ChathuraAbeygunawardhana <chathuraabeygunawardhana@gmail.com>
2024-12-09 20:42:26 +0530

    EA | Further developed the onboarding

--- components/OnboardingScreen.tsx
+++ components/OnboardingScreen.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+  Image,
+} from 'react-native';
 import { useOnboarding } from '../contexts/OnboardingContext';
 import { Colors } from '../constants/Colors';
 import { useTheme } from '../contexts/ThemeContext';
+import { useFonts } from 'expo-font';
 
 const OnboardingScreen = () => {
   const { isOnboardingVisible, setIsOnboardingVisible } = useOnboarding();
   const { theme } = useTheme();
   const currentColors = Colors[theme as 'light' | 'dark'];
+  const [fontsLoaded] = useFonts({
+    Airbnb: require('../assets/fonts/Airbnb.otf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   if (!isOnboardingVisible) return null;
 
@@ -17,12 +32,45 @@ const OnboardingScreen = () => {
       style={styles.backgroundImage}
     >
       <View style={styles.onboardingContainer}>
-        <TouchableOpacity
-          style={[styles.onboardingButton, { backgroundColor: currentColors.lightBlue }]}
-          onPress={() => setIsOnboardingVisible(false)}
-        >
-          <Text style={styles.onboardingButtonText}>get started</Text>
-        </TouchableOpacity>
+        <View style={styles.titleContainer}>
+          <Image
+            source={{
+              uri: 'https://i.ibb.co/tbpRpT5/Digital-Sketches-prev-ui.png',
+            }}
+            style={styles.headerImage}
+            id="shoe"
+          />
+          <Text
+            style={[
+              styles.onboardingTitle,
+              { fontFamily: 'Airbnb', marginBottom: 5 },
+            ]}
+            id="title"
+          >
+            Start Your
+          </Text>
+          <Text style={[styles.onboardingTitle, { fontFamily: 'Airbnb' }]}>
+            Journey With Us
+          </Text>
+          <Text
+            style={[styles.subtitle, { marginBottom: 10 }]}
+            id="subtitle"
+          >
+            Smart Gorgeous & Fashionable
+          </Text>
+          <Text style={styles.subtitle}>Collection</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={[
+              styles.onboardingButton,
+              { backgroundColor: currentColors.lightBlue },
+            ]}
+            onPress={() => setIsOnboardingVisible(false)}
+          >
+            <Text style={styles.onboardingButtonText}>Get started</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </ImageBackground>
   );
@@ -36,16 +84,44 @@ const styles = StyleSheet.create({
   onboardingContainer: {
     flex: 1,
     justifyContent: 'flex-end',
-    alignItems: 'flex-end',
     padding: 20,
   },
+  titleContainer: {
+    alignItems: 'flex-start',
+    flex: 1,
+    justifyContent: 'center',
+    marginTop: 200, // moved higher by reducing marginTop from 250 to 200
+  },
+  buttonContainer: {
+    alignItems: 'flex-end',
+  },
   onboardingButton: {
-    padding: 15,
-    borderRadius: 20,
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 50,
   },
   onboardingButtonText: {
     color: Colors.white,
     fontSize: 16,
+  },
+  onboardingTitle: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    marginBottom: 20,
+    textAlign: 'center',
+    marginLeft: -2,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: 'grey',
+    textAlign: 'center',
+    marginTop: 0,
+  },
+  headerImage: {
+    width: '100%',
+    height: 250, // increased from 200 to 250
+    resizeMode: 'contain',
+    bottom: 140,
   },
 });
 
